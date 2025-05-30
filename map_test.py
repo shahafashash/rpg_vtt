47b97a336ca264d92094fadcd3fdb31637385eec
simon Lab <simelav24@gmail.com>
2025-05-30 21:59:08 +0300

    cleanups

--- map_test.py
+++ map_test.py
@@ -1,4 +1,26 @@
 
+splash_text =r'''
+     _           _                                 
+    | |         | |                                
+  __| |_ __   __| |   ___ __ _ _ ____   ____ _ ___ 
+ / _` | '_ \ / _` |  / __/ _` | '_ \ \ / / _` / __|
+| (_| | | | | (_| | | (_| (_| | | | \ V / (_| \__ \
+ \__,_|_| |_|\__,_|  \___\__,_|_| |_|\_/ \__,_|___/
+   by Shahaf Ashash and Simon Labusnky  
+'''
+
+usage_text = '''
+usage:
+M - load map image
+T - add token or multiple at mouse position
+Ctrl C - copy selected token
+Ctrl V - paste copied token at mouse position
+G - toggle grid
+Ctrl S - save canvas state to file
+Ctrl L - load canvas state from file
+'''
+
+
 import json
 import os
 
@@ -20,6 +42,9 @@ from canvas_gui import CanvasGui
 if __name__ == "__main__":
     pygame.init()
     menu_gui.init()
+
+    print(splash_text)
+    print(usage_text)
 
     # pygame initializations
     message_queue = PublisherEventQueue()
@@ -60,20 +85,6 @@ if __name__ == "__main__":
             gui.handle_event(message)
             canvas.handle_event(message)
 
-			
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-                model = canvas.get_current_state()
-                with open('canvas_save.json', "w") as f:
-                    json.dump(model.model_dump(), f, indent=4)
-            
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_l:
-                if not os.path.exists('canvas_save.json'):
-                    continue
-                with open('canvas_save.json', 'r') as f:
-                    data = f.read()
-                model = CanvasModel.model_validate_json(data)
-                canvas.load_state(model)
-            
             if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
                 # Open a file dialog to browse for an image
                 root = tk.Tk()
@@ -121,19 +132,36 @@ if __name__ == "__main__":
                     number_pressed = event.key - pygame.K_0
                     token.add_decoration(font_decoration.render(str(number_pressed), True, (255, 255, 255)))
             
-            if event.type == pygame.KEYDOWN and event.mod & pygame.KMOD_CTRL:
                 if event.key == pygame.K_c:
+                    # copy selected token
                     token = canvas.get_selected_token()
                     if token is None:
                         continue
                     copied_token = token
                     print('copied')
+
                 elif event.key == pygame.K_v:
+                    # paste copied token
                     if copied_token is None:
                         continue
                     mouse_pos = Vector2(pygame.mouse.get_pos())
                     token_model = copied_token.get_current_state()
                     canvas.add_token(token_model.path, mouse_pos)
+
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                    # save canvas state to file
+                    model = canvas.get_current_state()
+                    with open('canvas_save.json', "w") as f:
+                        json.dump(model.model_dump(), f, indent=4)
+                
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+                    # load canvas state from file
+                    if not os.path.exists('canvas_save.json'):
+                        continue
+                    with open('canvas_save.json', 'r') as f:
+                        data = f.read()
+                    model = CanvasModel.model_validate_json(data)
+                    canvas.load_state(model)
         
 
             
